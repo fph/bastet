e508aecc061f58e50132524103d32cef417ff5db
Federico Poloni <f.poloni@sns.it>
2009-01-05 00:51:11 +0100

    Stub of BastetBlockChooser

--- BastetBlockChooser.cpp
+++ BastetBlockChooser.cpp
@@ -1,5 +1,23 @@
 #include "BastetBlockChooser.hpp"
 
+#include "Block.hpp"
+#include <cstdlib>
+
 namespace Bastet{
+  BastetBlockChooser::BastetBlockChooser(){
+
+  }
+  
+  BastetBlockChooser::~BastetBlockChooser(){
+
+  }
+
+  StartingSet BastetBlockChooser::ChooseStartingSet(){
+    return std::make_pair(&blocks[random()%7],&blocks[random()%7]);
+  }
+
+  Block *Choose(Well *well, Block* currentBlock){
+    return &blocks[random()%7];
+  }
 
 }
--- BastetBlockChooser.hpp
+++ BastetBlockChooser.hpp
@@ -1,7 +1,17 @@
 #ifndef BASTET_BLOCK_CHOOSER_HPP
 #define BASTET_BLOCK_CHOOSER_HPP
 
+#include "BlockChooser.hpp"
+
 namespace Bastet{
+  class BastetBlockChooser: public BlockChooser{
+  public:
+    BastetBlockChooser();
+    ~BastetBlockChooser();
+    virtual StartingSet ChooseStartingSet();
+    virtual Block *Choose(Well *well, Block *currentBlock);
+  private:
+  };
 
 }
 

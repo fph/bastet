e519504aac7f0c33700b5b5e42865b2eb1e5013e
Federico Poloni <f.poloni@sns.it>
2009-01-19 23:17:42 +0100

    README, AUTHORS, INSTALL and other license boilerplate

--- BlockPosition.hpp
+++ BlockPosition.hpp
@@ -1,3 +1,21 @@
+/*
+    Bastet - tetris clone with embedded bastard block chooser
+    (c) 2005-2009 Federico Poloni <f.polonithirtyseven@sns.it> minus 37
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef BLOCK_POSITION_HPP
 #define BLOCK_POSITION_HPP
 
